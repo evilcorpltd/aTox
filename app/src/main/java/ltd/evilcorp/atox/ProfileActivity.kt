fd1d0fc4ef8f78fe54baaa803d390d54727f88e4
Robin Lindén <dev@robinlinden.eu>
2019-05-12 21:57:27 +0200

    Start a Tox thread

--- app/src/main/java/ltd/evilcorp/atox/ProfileActivity.kt
+++ app/src/main/java/ltd/evilcorp/atox/ProfileActivity.kt
@@ -3,7 +3,24 @@ package ltd.evilcorp.atox
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import im.tox.tox4j.core.options.ProxyOptions
+import im.tox.tox4j.core.options.SaveDataOptions
+import im.tox.tox4j.core.options.ToxOptions
 import kotlinx.android.synthetic.main.activity_profile.*
+import java.lang.Thread.sleep
+import kotlin.concurrent.thread
+
+private const val HEX_CHARS = "0123456789ABCDEF"
+
+private fun String.hexToByteArray(): ByteArray {
+    val bytes = ByteArray(length / 2)
+
+    for (i in 0 until length step 2) {
+        bytes[i.shr(1)] = HEX_CHARS.indexOf(this[i]).shl(4).or(HEX_CHARS.indexOf(this[i + 1])).toByte()
+    }
+
+    return bytes
+}
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -16,6 +33,43 @@ class ProfileActivity : AppCompatActivity() {
             App.profile = if (username.text.isNotEmpty()) username.text.toString() else "aTox user"
             App.password = if (password.text.isNotEmpty()) password.text.toString() else ""
             startActivity(Intent(this@ProfileActivity, ContactListActivity::class.java))
+
+            thread(start = true) {
+                val tox = Tox(
+                    ToxOptions(
+                        true,
+                        true,
+                        true,
+                        ProxyOptions.`None$`(),
+                        0,
+                        0,
+                        0,
+                        SaveDataOptions.`None$`(),
+                        true
+                    )
+                )
+
+                tox.bootstrap(
+                    "tox.verdict.gg",
+                    33445,
+                    "1C5293AEF2114717547B39DA8EA6F1E331E5E358B35F9B6B5F19317911C5F976".hexToByteArray()
+                )
+                tox.bootstrap(
+                    "tox.kurnevsky.net",
+                    33445,
+                    "82EF82BA33445A1F91A7DB27189ECFC0C013E06E3DA71F588ED692BED625EC23".hexToByteArray()
+                )
+                tox.bootstrap(
+                    "tox.abilinski.com",
+                    33445,
+                    "10C00EB250C3233E343E2AEBA07115A5C28920E9C8D29492F6D00B29049EDC7E".hexToByteArray()
+                )
+
+                while (true) {
+                    sleep(tox.iterate().toLong())
+                }
+            }
+
             finish()
         }
     }
--- app/src/main/java/ltd/evilcorp/atox/Tox.kt
+++ app/src/main/java/ltd/evilcorp/atox/Tox.kt
@@ -1,26 +1,87 @@
 package ltd.evilcorp.atox
 
+import android.util.Log
+import im.tox.tox4j.core.callbacks.ToxCoreEventListener
+import im.tox.tox4j.core.enums.ToxConnection
+import im.tox.tox4j.core.enums.ToxFileControl
+import im.tox.tox4j.core.enums.ToxMessageType
+import im.tox.tox4j.core.enums.ToxUserStatus
 import im.tox.tox4j.impl.jni.ToxCoreImpl
 import im.tox.tox4j.core.options.ToxOptions
-import im.tox.tox4j.core.options.ProxyOptions
 
-class Tox {
+private class NoToxEventListener : ToxCoreEventListener<Int> {
+    override fun friendLosslessPacket(p0: Int, p1: ByteArray, p2: Int?): Int {
+        return Log.e("ToxCore", "friendLosslessPacket")
+    }
 
-    private var tox: ToxCoreImpl? = null
+    override fun fileRecvControl(p0: Int, p1: Int, p2: ToxFileControl, p3: Int?): Int {
+        return Log.e("ToxCore", "fileRecvControl")
+    }
 
-    fun init() {
-        val opts = ToxOptions(
-            true,
-            true,
-            true,
-            null,
-            0,
-            0,
-            0,
-            null,
-            true
-        )
+    override fun friendStatusMessage(p0: Int, p1: ByteArray, p2: Int?): Int {
+        return Log.e("ToxCore", "friendStatusMessage")
+    }
 
-        tox = ToxCoreImpl(opts)
+    override fun friendReadReceipt(p0: Int, p1: Int, p2: Int?): Int {
+        return Log.e("ToxCore", "friendReadReceipt")
+    }
+
+    override fun friendStatus(p0: Int, p1: ToxUserStatus, p2: Int?): Int {
+        return Log.e("ToxCore", "friendStatus")
+    }
+
+    override fun friendConnectionStatus(p0: Int, p1: ToxConnection, p2: Int?): Int {
+        return Log.e("ToxCore", "friendConnectionStatus")
+    }
+
+    override fun friendRequest(p0: ByteArray, p1: Int, p2: ByteArray, p3: Int?): Int {
+        return Log.e("ToxCore", "friendRequest")
+    }
+
+    override fun friendMessage(p0: Int, p1: ToxMessageType, p2: Int, p3: ByteArray, p4: Int?): Int {
+        return Log.e("ToxCore", "friendMessage")
+    }
+
+    override fun friendName(p0: Int, p1: ByteArray, p2: Int?): Int {
+        return Log.e("ToxCore", "friendName")
+    }
+
+    override fun fileRecvChunk(p0: Int, p1: Int, p2: Long, p3: ByteArray, p4: Int?): Int {
+        return Log.e("ToxCore", "fileRecvChunk")
+    }
+
+    override fun fileRecv(p0: Int, p1: Int, p2: Int, p3: Long, p4: ByteArray, p5: Int?): Int {
+        return Log.e("ToxCore", "fileRecv")
+    }
+
+    override fun friendLossyPacket(p0: Int, p1: ByteArray, p2: Int?): Int {
+        return Log.e("ToxCore", "friendLossyPacket")
+    }
+
+    override fun selfConnectionStatus(p0: ToxConnection, p1: Int?): Int {
+        return Log.e("ToxCore", "selfConnectionStatus")
+    }
+
+    override fun friendTyping(p0: Int, p1: Boolean, p2: Int?): Int {
+        return Log.e("ToxCore", "friendTyping")
+    }
+
+    override fun fileChunkRequest(p0: Int, p1: Int, p2: Long, p3: Int, p4: Int?): Int {
+        return Log.e("ToxCore", "fileChunkRequest")
+    }
+
+}
+
+class Tox(options: ToxOptions) {
+    private val tox: ToxCoreImpl = ToxCoreImpl(options)
+
+    fun bootstrap(address: String, port: Int, publicKey: ByteArray) {
+        tox.bootstrap(address, port, publicKey)
+        tox.addTcpRelay(address, port, publicKey)
+    }
+
+    fun iterate(): Int {
+        tox.iterate(NoToxEventListener(), null)
+        return tox.iterationInterval()
     }
 }
