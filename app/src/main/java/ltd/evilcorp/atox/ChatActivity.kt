86b8211a08cb4a0aa6d2357d25541034df4c1c6f
Robin Lindén <dev@robinlinden.eu>
2019-05-13 22:31:06 +0200

    Allow adding messages to the chat view

--- app/src/main/java/ltd/evilcorp/atox/ChatActivity.kt
+++ app/src/main/java/ltd/evilcorp/atox/ChatActivity.kt
@@ -4,8 +4,11 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_chat.*
 
 class ChatActivity : AppCompatActivity() {
+    private val messages = ArrayList<MessageModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
@@ -13,11 +16,18 @@ class ChatActivity : AppCompatActivity() {
         val username = intent.getStringExtra("username")
         Log.e("ChatActivity", username)
 
-        val listView = findViewById<ListView>(R.id.messages)
-        val messages = ArrayList<MessageModel>()
         messages.add(MessageModel("hello", Sender.Received))
         messages.add(MessageModel("how are you", Sender.Received))
         messages.add(MessageModel("I'm good, thanks.", Sender.Sent))
-        listView.adapter = MessagesAdapter(this, messages)
+
+        val adapter = MessagesAdapter(this, messages)
+        val messageView = findViewById<ListView>(R.id.messages)
+        messageView.adapter = adapter
+
+        send.setOnClickListener {
+            messages.add(MessageModel(outgoingMessage.text.toString(), Sender.Sent))
+            adapter.notifyDataSetChanged()
+            outgoingMessage.text.clear()
+        }
     }
 }
