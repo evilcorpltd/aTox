0a738a9be86c04ce46d3913aef249adbe95c3d3e
Robin Lindén <dev@robinlinden.eu>
2019-05-16 23:21:23 +0200

    Add a few example menu items

--- app/src/main/java/ltd/evilcorp/atox/ContactListActivity.kt
+++ app/src/main/java/ltd/evilcorp/atox/ContactListActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v4.widget.DrawerLayout
-import android.support.v7.app.AppCompatActivity
 import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.ListView
 import kotlinx.android.synthetic.main.contact_list_view_item.view.*
+import kotlin.random.Random
 
 class ContactListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,7 +42,8 @@ class ContactListActivity : AppCompatActivity() {
                 "23.09.17"
             )
         )
-        listView.adapter = ContactAdapter(this, contacts)
+        val adapter = ContactAdapter(this, contacts)
+        listView.adapter = adapter
 
         val drawer = findViewById<DrawerLayout>(R.id.drawerLayout)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
@@ -53,6 +57,31 @@ class ContactListActivity : AppCompatActivity() {
 
         drawer.addDrawerListener(drawerToggle)
         drawerToggle.syncState()
+
+        val menu = findViewById<ListView>(R.id.menuDrawer)
+        menu.setOnItemClickListener { _: AdapterView<*>, _: View, _: Int, id: Long ->
+            when (id) {
+                0L -> {
+                    contacts.add(
+                        ContactModel(
+                            "new EchoBot ${Random.nextInt(-1000, 1000)}",
+                            "76518406F6A9F2217E8DC487CC783C25CC16A15EB36FF32E335A235342C48A39218F515C39A6",
+                            "Never"
+                        )
+                    )
+                    adapter.notifyDataSetChanged()
+                }
+                1L -> startActivity(Intent(this, ContactListActivity::class.java))
+                2L -> finishAffinity()
+            }
+        }
+
+        val menuItems = ArrayList<String>()
+        menuItems.add("Add EchoBot")
+        menuItems.add("Add contact")
+        menuItems.add("Destroy everything")
+
+        menu.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, menuItems)
 
         toolbar.title = resources.getString(R.string.app_name)
         window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimaryDark)
