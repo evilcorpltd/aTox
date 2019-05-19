c314e21000626bf93b5f640314a1fd85abe28d8c
Robin Lindén <dev@robinlinden.eu>
2019-05-19 19:15:18 +0200

    Make sidebar better

--- app/src/main/java/ltd/evilcorp/atox/activity/ContactListActivity.kt
+++ app/src/main/java/ltd/evilcorp/atox/activity/ContactListActivity.kt
@@ -2,16 +2,16 @@ package ltd.evilcorp.atox.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.contact_list_view_item.view.*
 import ltd.evilcorp.atox.App
 import ltd.evilcorp.atox.ContactAdapter
@@ -19,12 +19,20 @@ import ltd.evilcorp.atox.ContactModel
 import ltd.evilcorp.atox.R
 import kotlin.random.Random
 
-class ContactListActivity : AppCompatActivity() {
+class ContactListActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private val contactAdapter by lazy { ContactAdapter(this, App.contacts) }
+    private val navigationHeader by lazy { findViewById<NavigationView>(R.id.navView).getHeaderView(0) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_list)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        val listView = findViewById<ListView>(R.id.contactList)
+        val name = navigationHeader.findViewById<TextView>(R.id.profileName)
+        name.text = App.profile
+
+        val contactList = findViewById<ListView>(R.id.contactList)
         App.contacts.add(
             ContactModel(
                 "EchoBot",
@@ -49,54 +57,17 @@ class ContactListActivity : AppCompatActivity() {
                 0
             )
         )
+        contactList.adapter = contactAdapter
 
-        val adapter = ContactAdapter(this, App.contacts)
-        listView.adapter = adapter
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView: NavigationView = findViewById(R.id.navView)
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
-        val drawer = findViewById<DrawerLayout>(R.id.drawerLayout)
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        val drawerToggle = object : ActionBarDrawerToggle(
-            this,
-            drawer,
-            toolbar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
-        ) {}
-
-        drawer.addDrawerListener(drawerToggle)
-        drawerToggle.syncState()
-
-        val menu = findViewById<ListView>(R.id.menuDrawer)
-        menu.setOnItemClickListener { _: AdapterView<*>, _: View, _: Int, id: Long ->
-            when (id) {
-                0L -> {
-                    App.contacts.add(
-                        ContactModel(
-                            "new EchoBot ${Random.nextInt(-1000, 1000)}",
-                            "76518406F6A9F2217E8DC487CC783C25CC16A15EB36FF32E335A235342C48A39218F515C39A6",
-                            "Never",
-                            0
-                        )
-                    )
-                    adapter.notifyDataSetChanged()
-                }
-                1L -> startActivity(Intent(this, AddContactActivity::class.java))
-                2L -> finishAffinity()
-            }
-
-            val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
-            drawerLayout.closeDrawer(GravityCompat.START)
-        }
-
-        val menuItems = ArrayList<String>()
-        menuItems.add("Add EchoBot")
-        menuItems.add("Add contact")
-        menuItems.add("Destroy everything")
-
-        menu.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, menuItems)
-
-        toolbar.title = resources.getString(R.string.app_name)
-        window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimaryDark)
+        navView.setNavigationItemSelectedListener(this)
     }
 
     override fun onBackPressed() {
@@ -104,8 +75,36 @@ class ContactListActivity : AppCompatActivity() {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START)
         } else {
-            moveTaskToBack(true)
+            super.onBackPressed()
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_contact -> {
+                startActivity(Intent(this, AddContactActivity::class.java))
+            }
+            R.id.add_echobot -> {
+                App.contacts.add(
+                    ContactModel(
+                        "new EchoBot ${Random.nextInt(-1000, 1000)}",
+                        "76518406F6A9F2217E8DC487CC783C25CC16A15EB36FF32E335A235342C48A39218F515C39A6",
+                        "Never",
+                        0
+                    )
+                )
+                contactAdapter.notifyDataSetChanged()
+            }
+            R.id.settings -> {
+                // TODO(robinlinden): Settings activity
+            }
+            R.id.destroy_everything -> {
+                finishAffinity()
+            }
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 
     fun openChat(view: View) {
@@ -114,7 +113,7 @@ class ContactListActivity : AppCompatActivity() {
 
         //TODO(endoffile78) figure out a better way to get the friend number
         var friendNumber = 0
-        App.contacts.forEach() lit@{
+        App.contacts.forEach lit@{
             if (it.name == view.name.text) {
                 friendNumber = it.friendNumber
                 return@lit // break
