8520e8a874200a8441c21944600446db3f6d8f8c
Robin Lindén <dev@robinlinden.eu>
2019-05-20 20:30:35 +0200

    Disable send button if there's no message to send

--- app/src/main/java/ltd/evilcorp/atox/activity/ChatActivity.kt
+++ app/src/main/java/ltd/evilcorp/atox/activity/ChatActivity.kt
@@ -1,6 +1,8 @@
 package ltd.evilcorp.atox.activity
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -29,6 +31,15 @@ class ChatActivity : AppCompatActivity() {
             adapter.notifyDataSetChanged()
             outgoingMessage.text.clear()
         }
+        send.isEnabled = false
+
+        outgoingMessage.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                send.isEnabled = s!!.toString().isNotEmpty()
+            }
+        })
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         toolbar.title = intent.getStringExtra("username")
