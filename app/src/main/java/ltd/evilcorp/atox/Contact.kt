4c7ae3301ada5b2cb080c4700a737c748bccdd5d
Robin Lindén <dev@robinlinden.eu>
2019-05-25 13:19:19 +0200

    Turn Contact into a data class

--- app/src/main/java/ltd/evilcorp/atox/Contact.kt
+++ app/src/main/java/ltd/evilcorp/atox/Contact.kt
@@ -4,7 +4,7 @@ package ltd.evilcorp.atox
 import im.tox.tox4j.core.enums.ToxConnection
 import im.tox.tox4j.core.enums.ToxUserStatus
 
-class Contact(
+data class Contact(
     val publicKey: ByteArray,
     var friendNumber: Int = 0,
     var name: String = "Unknown",
@@ -13,4 +13,34 @@ class Contact(
     var connectionStatus: ToxConnection = ToxConnection.NONE,
     var lastMessage: String = "Never",
     var typing: Boolean = false
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Contact
+
+        if (!publicKey.contentEquals(other.publicKey)) return false
+        if (friendNumber != other.friendNumber) return false
+        if (name != other.name) return false
+        if (status != other.status) return false
+        if (statusMessage != other.statusMessage) return false
+        if (connectionStatus != other.connectionStatus) return false
+        if (lastMessage != other.lastMessage) return false
+        if (typing != other.typing) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = publicKey.contentHashCode()
+        result = 31 * result + friendNumber
+        result = 31 * result + name.hashCode()
+        result = 31 * result + status.hashCode()
+        result = 31 * result + statusMessage.hashCode()
+        result = 31 * result + connectionStatus.hashCode()
+        result = 31 * result + lastMessage.hashCode()
+        result = 31 * result + typing.hashCode()
+        return result
+    }
+}
