41fb92f0f6db87a30ca05f93e062ebbced53d476
Robin Lindén <dev@robinlinden.eu>
2019-05-31 01:27:58 +0200

    Reuse view for messages if possible

--- app/src/main/java/ltd/evilcorp/atox/ui/MessageAdapter.kt
+++ app/src/main/java/ltd/evilcorp/atox/ui/MessageAdapter.kt
@@ -10,29 +10,41 @@ import ltd.evilcorp.atox.R
 import ltd.evilcorp.atox.vo.MessageModel
 import ltd.evilcorp.atox.vo.Sender
 
+private fun inflateView(type: Sender, inflater: LayoutInflater): View =
+    inflater.inflate(
+        if (type == Sender.Sent) R.layout.message_sent else R.layout.message_received,
+        null,
+        true
+    )
+
 class MessagesAdapter(private val context: Context, private val messages: MutableList<MessageModel>) : BaseAdapter() {
     override fun getCount(): Int = messages.size
     override fun getItem(position: Int): Any = messages[position]
     override fun getItemId(position: Int): Long = position.toLong()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        // TODO(robinlinden): Optimise this.
-        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val view = inflater.inflate(
-            if (messages[position].sender == Sender.Sent) R.layout.message_sent else R.layout.message_received,
-            null,
-            true
-        )
+        val message = messages[position]
 
-        val vh = ViewHolder(view)
-        view.tag = vh
+        val view: View
+        val vh: ViewHolder
 
-        vh.message.text = messages[position].message
+        if (convertView != null && (convertView.tag as? ViewHolder)?.sender == message.sender) {
+            view = convertView
+            vh = view.tag as ViewHolder
+        } else {
+            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
-        return view!!
+            view = inflateView(message.sender, inflater)
+            vh = ViewHolder(view, message.sender)
+            view.tag = vh
+        }
+
+        vh.message.text = message.message
+
+        return view
     }
 
-    private class ViewHolder(row: View) {
+    private class ViewHolder(row: View, val sender: Sender) {
         val message = row.findViewById(R.id.message) as TextView
     }
 }
