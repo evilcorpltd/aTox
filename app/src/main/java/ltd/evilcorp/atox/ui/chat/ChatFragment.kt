e7ea3d5150ced3a9f63580f5ca54c1b07d41e510
Robin Lindén <dev@robinlinden.eu>
2019-07-25 00:19:31 +0200

    Allow copying chat messages

--- app/src/main/java/ltd/evilcorp/atox/ui/chat/ChatFragment.kt
+++ app/src/main/java/ltd/evilcorp/atox/ui/chat/ChatFragment.kt
@@ -1,16 +1,19 @@
 package ltd.evilcorp.atox.ui.chat
 
 import android.app.AlertDialog
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.AdapterView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.chat_fragment.*
 import kotlinx.android.synthetic.main.chat_fragment.view.*
 import kotlinx.android.synthetic.main.profile_image_layout.*
 import ltd.evilcorp.atox.R
@@ -18,6 +21,7 @@ import ltd.evilcorp.atox.ui.MessagesAdapter
 import ltd.evilcorp.atox.ui.colorByStatus
 import ltd.evilcorp.atox.vmFactory
 import ltd.evilcorp.atox.vo.ConnectionStatus
+import ltd.evilcorp.atox.vo.Message
 
 const val CONTACT_PUBLIC_KEY = "publicKey"
 
@@ -75,7 +79,7 @@ class ChatFragment : Fragment() {
 
         val adapter = MessagesAdapter(inflater)
         messages.adapter = adapter
-
+        registerForContextMenu(messages)
         viewModel.messages.observe(viewLifecycleOwner, Observer {
             adapter.messages = it
             adapter.notifyDataSetChanged()
@@ -96,6 +100,34 @@ class ChatFragment : Fragment() {
                 updateSendButton(this@apply)
             }
         })
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu,
+        v: View,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        when (v.id) {
+            R.id.messages -> {
+                requireActivity().menuInflater.inflate(R.menu.chat_message_context_menu, menu)
+            }
+        }
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val info = item.menuInfo as AdapterView.AdapterContextMenuInfo
+        return when (item.itemId) {
+            R.id.copy -> {
+                val clipboard = requireActivity().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                val message = messages.adapter.getItem(info.position) as Message
+                clipboard.primaryClip = ClipData.newPlainText(getText(R.string.message), message.message)
+
+                Toast.makeText(requireContext(), getText(R.string.copied), Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onContextItemSelected(item)
+        }
     }
 
     private fun updateSendButton(layout: View) {
