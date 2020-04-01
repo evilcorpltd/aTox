1a65dbdb6de3105582d284f971212b6298097646
Robin Linden <dev@robinlinden.eu>
2020-04-01 21:15:01 +0200

    Release 0.2.0

--- atox/build.gradle.kts
+++ atox/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ltd.evilcorp.atox"
         minSdkVersion(AndroidSdk.minVersion)
         targetSdkVersion(AndroidSdk.targetVersion)
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
         vectorDrawables.useSupportLibrary = true
