da95cec20802379edd06438423715cca231c5572
Robin Linden <dev@robinlinden.eu>
2020-05-02 14:35:19 +0200

    Fix aTox appearing as a browser

--- atox/src/main/kotlin/MainActivity.kt
+++ atox/src/main/kotlin/MainActivity.kt
@@ -1,12 +1,20 @@
 package ltd.evilcorp.atox
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceManager
 import dagger.android.AndroidInjection
+import kotlinx.android.synthetic.main.activity_main.*
 import ltd.evilcorp.atox.di.ViewModelFactory
 import javax.inject.Inject
+
+private const val TAG = "MainActivity"
+private const val SCHEME = "tox:"
+private const val TOX_ID_LENGTH = 76
 
 class MainActivity : AppCompatActivity() {
     @Inject
@@ -22,5 +30,20 @@ class MainActivity : AppCompatActivity() {
         )
 
         setContentView(R.layout.activity_main)
+
+        // Handle potential tox link.
+        if (intent.action == Intent.ACTION_VIEW) {
+            val data = intent.dataString ?: ""
+            Log.i(TAG, "Got uri with data: $data")
+            if (!data.startsWith(SCHEME) || data.length != SCHEME.length + TOX_ID_LENGTH) {
+                Log.e(TAG, "Got malformed uri: $data")
+                return
+            }
+
+            nav_host_fragment.findNavController().navigate(
+                R.id.action_contactListFragment_to_addContactFragment,
+                Bundle().apply { putString("toxId", data.drop(SCHEME.length)) }
+            )
+        }
     }
 }
