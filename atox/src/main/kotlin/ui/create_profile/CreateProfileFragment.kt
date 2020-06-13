1246543aa4fcd1407de916ef853a975fd22d0147
Robin Linden <dev@robinlinden.eu>
2020-06-13 16:28:43 +0200

    Fix back button on the create profile screen

--- atox/src/main/kotlin/ui/create_profile/CreateProfileFragment.kt
+++ atox/src/main/kotlin/ui/create_profile/CreateProfileFragment.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.WindowInsets
 import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -42,6 +43,10 @@ class CreateProfileFragment : Fragment() {
             )
 
             insets
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            activity?.finish()
         }
 
         btnCreate.setOnClickListener {
