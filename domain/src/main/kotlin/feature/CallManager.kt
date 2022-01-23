evilcorpltd/aTox

commit f4979ffd87f0f70b0fa62ce4f411207cdf50b48b

Fix call rejection during ongoing call breaking the call state

domain/src/main/kotlin/feature/CallManager.kt
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2021 aTox contributors
+// SPDX-FileCopyrightText: 2021-2022 aTox contributors
 //
 // SPDX-License-Identifier: GPL-3.0-only
 
@@ -81,8 +81,10 @@ class CallManager @Inject constructor(
     }
 
     fun endCall(publicKey: PublicKey) {
-        audioManager?.mode = AudioManager.MODE_NORMAL
-        _inCall.value = CallState.NotInCall
+        if ((inCall.value as CallState.InCall?)?.publicKey == publicKey) {
+            audioManager?.mode = AudioManager.MODE_NORMAL
+            _inCall.value = CallState.NotInCall
+        }
 
         removePendingCall(publicKey)
 

commit c561215e2c544e5cd0f66427c8f3e45f86394a18

Fix crash in handling of ending incoming calls
Introduced in f4979ffd87f0f70b0fa62ce4f411207cdf50b48b.

domain/src/main/kotlin/feature/CallManager.kt
@@ -81,7 +81,8 @@ class CallManager @Inject constructor(
     }
 
     fun endCall(publicKey: PublicKey) {
-        if ((inCall.value as CallState.InCall?)?.publicKey == publicKey) {
+        val state = inCall.value
+        if (state is CallState.InCall && state.publicKey == publicKey) {
             audioManager?.mode = AudioManager.MODE_NORMAL
             _inCall.value = CallState.NotInCall
         }