de7716984865cd21aff9eeb1d99c91baa8e626d6
Martin Maartensson <mail@mama.sh>
2022-02-25 00:08:11 +0100

    Add depenendcies for splash screen, jetpack destinations and jetpack compose

--- atox/build.gradle.kts
+++ atox/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 
     id(BuildPlugin.kotlinAndroid)
     id(BuildPlugin.kotlinKapt)
+    id("com.google.devtools.ksp") version "1.5.31-1.0.1" // Depends on your kotlin version
 }
 
 apply<KtlintPlugin>()
@@ -53,6 +54,7 @@ dependencies {
     implementation(project(":domain"))
 
     implementation(AndroidX.Core.ktx)
+    implementation("androidx.core:core-splashscreen:1.0.0-beta01")
     implementation(AndroidX.activity)
     implementation(AndroidX.appcompat)
     implementation(AndroidX.constraintlayout)
@@ -74,6 +76,14 @@ dependencies {
     implementation(AndroidX.Lifecycle.service)
     implementation(AndroidX.Lifecycle.viewmodelKtx)
 
+    val compose_version = "1.1.0"
+
+    implementation("androidx.compose.ui:ui:$compose_version")
+    implementation("androidx.compose.material:material:$compose_version")
+    implementation("androidx.compose.ui:ui-tooling-preview:$compose_version")
+    implementation("io.github.raamcosta.compose-destinations:core:1.3.1-beta")
+    ksp("io.github.raamcosta.compose-destinations:ksp:1.3.1-beta")
+    implementation("androidx.activity:activity-compose:1.4.0")
     implementation(Google.dagger)
     kapt(Google.Dagger.compiler)
 
