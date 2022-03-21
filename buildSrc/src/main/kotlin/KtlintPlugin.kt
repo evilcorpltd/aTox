9d93bce7938415d25f6555616c1c02e82994beef
Robin Linden <dev@robinlinden.eu>
2022-03-21 22:25:37 +0100

    Update ktlint

--- buildSrc/src/main/kotlin/KtlintPlugin.kt
+++ buildSrc/src/main/kotlin/KtlintPlugin.kt
@@ -2,7 +2,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.JavaExec
 
-private const val KTLINT_VERSION = "0.44.0"
+private const val KTLINT_VERSION = "0.45.1"
 private const val KTLINT = "com.pinterest:ktlint:$KTLINT_VERSION"
 
 class KtlintPlugin : Plugin<Project> {
